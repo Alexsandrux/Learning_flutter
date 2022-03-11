59b6ebee13e597b69447c3309183b38e2b76fc52
Paducel Alexandru <paducelalexandru@gmail.com>
2022-03-11 19:30:47 +0200

    converting my app to a stateful widget

--- flutter_app_from_scratch_1/lib/main.dart
+++ flutter_app_from_scratch_1/lib/main.dart
@@ -7,14 +7,61 @@ void main() {
   // si va incerca sa il afiseze pe ecran
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+// stateful widget este formata din doua clase, cea de a doua avand de obicei denumirea primei clase la care se adauga cuvantul "State" ca si conventie
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   // clasa mosteneste StatelessWidget.
   // fiecare widget din flutter trebuie sa extinda StalessWidget sau StatefulWidget care te obliga sa adaugi metoda build care va fi automat apelata de flutter
+  var answerIndex = 0;
+
+  void changeIndex(num newNumber) {
+    setState(() {
+      answerIndex = newNumber;
+    });
+    print(answerIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var answers = [
+      'Waiting for answer..',
+      'Good answer',
+      'Bad answer',
+    ];
+
     return MaterialApp(
       // clasa de baza care foloseste o combinatie de widgeturi furnizata de developerii flutter pentru a crea aplicatia
-      home: Text('Hello!'),
+      home: Scaffold(
+        // scaffold este un layout care ne ajuta sa facem aplicatii mai prezentabile, are o aparenta clasica android
+        appBar: AppBar(
+          title: Text('My first flutter scratch app'),
+        ),
+        body: Column(
+          children: [
+            Text('What is 2 + 3'),
+            ElevatedButton(
+              onPressed: () => changeIndex(1),
+              child: Text('5'),
+            ),
+            ElevatedButton(
+              onPressed: () => changeIndex(2),
+              child: Text('6'),
+            ),
+            ElevatedButton(
+              onPressed: () => changeIndex(2),
+              child: Text('8'),
+            ),
+            Text(answers.elementAt(answerIndex))
+          ],
+        ),
+      ),
       // home este un core widget care va fi adaugat de flutter automat pe ecran
     );
   }
